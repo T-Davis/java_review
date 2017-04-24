ff380761087550cf9480e8dba415aef9f239a60b
Trevor Davis <tzendou@gmail.com>
2017-04-24 15:34:51 +0800

    progress on basics

--- src/main/java/com/trevor/Basics.java
+++ src/main/java/com/trevor/Basics.java
@@ -2,7 +2,7 @@ package main.java.com.trevor;
 
 public class Basics {
 
-    //for loop
+    //for loop, if else ladder
     void forLoop(int x) {
         for (int i = 0; i < x; i++) {
             if (i == x) System.out.println("We're done!");
@@ -12,7 +12,15 @@ public class Basics {
         }
     }
 
-    //while and do while loops
+    //for, enhanced for loops, casting
+    public void forLoops() {
+        char[] chars = new char[10];
+        int h = 72;
+        for(int i = h; i < chars.length + h; i++) chars[i - h] = (char) i;
+        for(char x: chars) System.out.println(x);
+    }
+
+    //while, do while loops
     void whileLoops(int x) {
         if (x < 0) x *= x;
         while (x > 10) x /= 2;
@@ -20,6 +28,16 @@ public class Basics {
             System.out.println("x now equals " + x);
             x--;
         } while (x > 1);
+    }
+
+    //while loop
+    public void whileLoop() {
+        int thisVar = 0;
+        int thatVar = 5;
+        while(thisVar < thatVar) {
+            System.out.println("this is less than that");
+            thisVar++;
+        }
     }
 
     //enhanced for loop
@@ -54,7 +72,20 @@ public class Basics {
         }
     }
 
-    //casting
+    //switch statement, casting
+    public void switchIt() {
+        int a = (int) (Math.random() * 10);
+        switch (a) {
+            case 0: System.out.println("a = " + 0); break;
+            case 1: System.out.println("a = " + 1); break;
+            case 2: System.out.println("a = " + 2); break;
+            case 3: System.out.println("a = " + 3); break;
+            case 4: System.out.println("a = " + 4); break;
+            default: System.out.printf("a >= 5"); break;
+        }
+    }
+
+    //casting, while loop
     void numsToChars(int x) {
         if (x < 0) x *= x;
         while (x > 255) x /= 2;
